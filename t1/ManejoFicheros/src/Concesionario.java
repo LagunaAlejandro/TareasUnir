8d4537c68bda6d8ced2e2e9f7f6e39bc2f73832c
Alejandro Laguna rebolo <118814965+LagunaAlejandro@users.noreply.github.com>
2024-10-15 12:37:02 +0200

    update

--- t1/ManejoFicheros/src/Concesionario.java
+++ t1/ManejoFicheros/src/Concesionario.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+
 public class Concesionario {
+
+    private ArrayList<Coche> coches;
+
+    public Concesionario() {
+        coches = new ArrayList<>();
+        cargarDatos();
+    }
+public void cargarDatos() {
+        
+}
+
 }
--- t1/ManejoFicheros/src/Main.java
+++ t1/ManejoFicheros/src/Main.java
@@ -1,7 +1,51 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
+
 public class Main {
+
+
+
+
     public static void main(String[] args) {
 
+
+            Scanner scanner = new Scanner(System.in);
+            int opcion;
+
+            do {
+                System.out.println("\nMenú:");
+                System.out.println("1.- Añadir nuevo coche");
+                System.out.println("2.- Borrar coche por id");
+                System.out.println("3.- Consulta coche por id");
+                System.out.println("4.- Listado de coches");
+                System.out.println("5.- Terminar el programa");
+                System.out.print("Seleccione una opción: ");
+                opcion = scanner.nextInt();
+                scanner.nextLine();
+
+                switch (opcion) {
+                    case 1:
+
+                        break;
+                    case 2:
+
+                        break;
+                    case 3:
+
+                        break;
+                    case 4:
+
+                        break;
+                    case 5:
+                        System.out.println("Saliendo...");
+                        scanner.close();
+                        break;
+
+                    default:
+                        System.out.println("Opción inválida. Por favor, seleccione una opción del menú.");
+                        break;
+                }
+            } while (opcion != 5);
         }
     }
+
